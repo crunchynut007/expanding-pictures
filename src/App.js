412e6e2115a133fc6a1658b6c1d4476b68dfa0e2
Mischel <kandiahmischel@gmail.com>
2022-02-02 21:19:47 +1030

    Refactored to lift state to Layout component instead of Card component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import './App.css';
-import Card from "./components/card";
+import Layout from "./components/Layout";
 
 
 function App() {
   return (
     <div className="body-wrap">
-        <div className='container'>
-            <Card text='Wildlife' url='https://images.unsplash.com/photo-1643732660623-4b7868af1216?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80'/>
-            <Card text='Autumn' url='https://images.unsplash.com/photo-1643722571244-91033408e1b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'/>
-            <Card text='Lakeside Winter' url='https://images.unsplash.com/photo-1643721859448-4d241d04448b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'/>
-            <Card text='The Dock' url='https://images.unsplash.com/photo-1643718132395-9359e4615236?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'/>
-            <Card text='Snowy Mountain Road' url='https://images.unsplash.com/photo-1643653773680-eb00565bcbf9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'/>
-            <Card text='Bridges' url='https://images.unsplash.com/photo-1643574914412-409598704135?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'/>
-        </div>
+        <Layout />
     </div>
   );
 }
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -5,20 +5,22 @@ import './card.css';
 class Card extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            activePanel: '',
-        };
-
         this.handleClick = this.handleClick.bind(this);
     }
 
     handleClick() {
-        this.state.activePanel === 'active' ? this.setState({activePanel: ''}) : this.setState({activePanel: "active"});
+        //clears all values in the array and assigns a single active class. Passed to the parent state object
+        let newActive = this.props.activePanel;
+        for (let x=0; x<newActive.length;x++) {
+            newActive[x]='';
+        }
+        newActive[this.props.index] = 'active';
+        this.props.onClickHandler(newActive)
     }
 
     render() {
         return (
-            <div className={'panel ' + this.state.activePanel} style={{ backgroundImage:`url(${this.props.url})` }} onClick={this.handleClick}>
+            <div className={'panel ' + this.props.activePanel[this.props.index]} style={{ backgroundImage:`url(${this.props.url})` }} onClick={this.handleClick}>
                 <h3>{this.props.text}</h3>
             </div>
         );
